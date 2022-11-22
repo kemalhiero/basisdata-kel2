35b96f4a247e527db94aef0be8dfa00f5f5f6dca
Kemal muhammad <kemalhiero@gmail.com>
2022-11-22 09:45:36 +0700

    up asset template

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        return view('index');
+    }
+
+    public function login()
+    {
+        return view('login');
     }
 }
